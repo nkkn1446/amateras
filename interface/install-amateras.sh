bc87d274fe2b2c06d79dbf11fd8c4d44cbf6315b
nkkn1446 <46309957+nkkn1446@users.noreply.github.com>
2019-08-18 14:40:01 +0900

    インストールシェル更新

--- interface/install-amateras.sh
+++ interface/install-amateras.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#sudo apt-get install autoconf automake libtool curl make g++ unzip apache2 -y
+#sudo apt-get install autoconf automake libtool curl make g++ unzip git -y
 #git clone https://github.com/protocolbuffers/protobuf.git
 
 #cd protobuf
@@ -8,9 +8,15 @@
 #./autogen.sh
 #./configure
 #make -j2
-#make -j2 check
+#make check
 #sudo make -j2 install
 #sudo ldconfig
+
+#git clone https://github.com/grpc/grpc-web
+
+#cd grpc-web
+#sudo apt-get install libprotoc-dev
+#sudo make -j2 install-plugin
 
 #echo "#!/bin/bash\nexport GOROOT=$HOME/go\nexport GOPATH=$HOME/.go\nexport PATH=$PATH:$HOME/go/bin:$HOME/.go/bin">~/.bash_profile
 #wget https://dl.google.com/go/go1.11.9.linux-amd64.tar.gz
@@ -23,7 +29,6 @@
 #cd ../..
 #mv go ~/go
 #rm -fr ~/go1.4
-#go get -u google.golang.org/grpc
 
 #sudo apt install -y nodejs npm
 #sudo npm install n -g
@@ -39,26 +44,8 @@
 #cd ~/.go/src/protobuf
 #make -j2
 
-#aarch64
 #sudo apt install docker-compose -y
-
-#x86-x64
-#wget "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -O docker-compose
-#sudo mv docker-compose /usr/local/bin/
-#sudo chmod +x /usr/local/bin/docker-compose
 #sudo apt install docker.io -y
-
-#git clone https://github.com/grpc/grpc-web
-
-#cd grpc-web
-#git checkout 5852e276e096795a363dd65312b971393655618d
-#sudo apt-get install libprotoc-dev
-#sudo make -j2 install-plugin
-
-#https://github.com/grpc/grpc-web/tree/master/net/grpc/gateway/examples/helloworld
-#cd net/grpc/gateway/examples/helloworld/
-#sudo docker build -t helloworld/envoy -f ./envoy.Dockerfile .
-#sudo docker run -d -p 8080:8080 -p 9901:9901 --network=host helloworld/envoy
 
 #curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 #echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
@@ -80,6 +67,15 @@
 #./gen.sh
 #sudo docker-compose up --build -d server proxy
 
+#go get -u google.golang.org/grpc
+
+#https://github.com/grpc/grpc-web/tree/master/net/grpc/gateway/examples/helloworld
+#cd grpc-web/net/grpc/gateway/examples/helloworld/
+#docker build -t helloworld/envoy -f ./envoy.Dockerfile .
+#docker run -d -p 8080:8080 -p 9901:9901 --network=host helloworld/envoy
+
 #必要に応じて実行
 #go get -u github.com/golang/protobuf/protoc-gen-go
 #go get -u github.com/pilu/fresh
+
+#git clone https://github.com/totsuan/amateras.git
