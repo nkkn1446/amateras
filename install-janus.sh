282db60120c2da2c14391a95f47889cfff11bec3
totsuan <nkkn1446count10@gmail.com>
2019-09-21 08:30:21 +0900

    janusno

---
+++ install-janus.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#https://github.com/totsuan/janus-gateway/tree/develop
+
+#sudo apt install gtk-doc-tools \
+#	aptitude \
+#	git \
+#	-y
+
+#sudo aptitude install libmicrohttpd-dev libjansson-dev \
+#	libssl1.0-dev libsrtp-dev libsofia-sip-ua-dev libglib2.0-dev \
+#	libopus-dev libogg-dev libcurl4-openssl-dev liblua5.3-dev \
+#	libconfig-dev pkg-config gengetopt libtool automake -y
+
+#git clone https://gitlab.freedesktop.org/libnice/libnice
+#
+#./autogen.sh
+#./configure --prefix=/usr
+#make -j2 && sudo make install
+
+#wget https://github.com/cisco/libsrtp/archive/v1.5.4.tar.gz
+#tar xfv v1.5.4.tar.gz
+
+#./configure --prefix=/usr --enable-openssl
+#make shared_library -j2 && sudo make install
+
+#wget https://github.com/cisco/libsrtp/archive/v2.2.0.tar.gz
+#tar xfv v2.2.0.tar.gz
+
+#./configure --prefix=/usr --enable-openssl
+#make shared_library -j2 && sudo make install
+
+#sudo aptitude install graphviz -y
+
+#ubuntu bionic
+#sudo apt cmake install bison flex -y
+#tar xvf doxygen-1.8.11.src.tar.gz
+
+#mkdir build
+#cd build/
+#cmake -G "Unix Makefiles" ..
+#make -j2 && sudo make install
+
+
+#git clone https://github.com/totsuan/janus-gateway.git
+
+#sh autogen.sh
+#./configure --prefix=/opt/janus --disable-websockets --disable-data-channels --disable-rabbitmq --disable-mqtt --enable-docs
+#make -j2
+#sudo make install
+#sudo make configs
