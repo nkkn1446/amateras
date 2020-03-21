530c37a5822cb1261eedcd79acf01e904cacb5a6
totsuan <nkkn1446count10@gmail.com>
2020-03-21 14:07:54 +0900

    fix

--- interface/install-amateras.sh
+++ interface/install-amateras.sh
@@ -4,21 +4,20 @@
 # protobuf install
 #
 
-#sudo apt-get install autoconf automake libtool curl make g++ unzip git -y
-
-#git clone https://github.com/protocolbuffers/protobuf.git
-#cd protobuf
-#git submodule update --init --recursive
-#./autogen.sh
-#./configure
-#make -j2
-#make check
-#sudo make -j2 install
-#sudo ldconfig
+sudo apt-get install autoconf automake libtool curl make g++ unzip git -y
+git clone https://github.com/protocolbuffers/protobuf.git
+cd protobuf
+git submodule update --init --recursive
+./autogen.sh
+./configure
+make -j2
+make check
+sudo make -j2 install
+sudo ldconfig
 # server
-#snap install go
-#echo "#!/bin/bash\nexport GOROOT=$HOME/go\nexport GOPATH=$HOME/.go\nexport PATH=$PATH:$HOME/go/bin:$HOME/.go/bin">~/.bash_profile
-#go get -u github.com/golang/protobuf/protoc-gen-go
+snap install go
+echo "#!/bin/bash\nexport GOROOT=$HOME/go\nexport GOPATH=$HOME/.go\nexport PATH=$PATH:$HOME/go/bin:$HOME/.go/bin">~/.bash_profile
+go get -u github.com/golang/protobuf/protoc-gen-go
 # html
 #git clone https://github.com/grpc/grpc-web
 #cd grpc-web
