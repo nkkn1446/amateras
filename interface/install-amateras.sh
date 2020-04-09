d6a417b2ad7ce57d9b4fae209eb791af24626d6c
nkkn1446 <46309957+nkkn1446@users.noreply.github.com>
2020-04-09 17:49:59 +0900

    fix

--- interface/install-amateras.sh
+++ interface/install-amateras.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
 
 #
+# momo install
+#
+#sudo apt install libsdl2-dev -y
+
+#
 # node install
 #
-
-npm install
+#sudo apt install npm
+#npm install
 
 #
 # protobuf install
 #
-
-sudo apt-get install autoconf automake libtool curl make g++ unzip git -y
-git clone https://github.com/protocolbuffers/protobuf.git
-cd protobuf
-git submodule update --init --recursive
-./autogen.sh
-./configure
-make -j2
-make check
-sudo make -j2 install
-sudo ldconfig
+#sudo apt-get install autoconf automake libtool curl make g++ unzip git -y
+#git clone https://github.com/protocolbuffers/protobuf.git
+#cd protobuf
+#git submodule update --init --recursive
+#./autogen.sh
+#./configure
+#make -j2
+#make check
+#sudo make -j2 install
+#sudo ldconfig
 # server
-snap install go
-echo "#!/bin/bash\nexport GOROOT=$HOME/go\nexport GOPATH=$HOME/.go\nexport PATH=$PATH:$HOME/go/bin:$HOME/.go/bin">~/.bash_profile
-go get -u github.com/golang/protobuf/protoc-gen-go
-# html
-#git clone https://github.com/grpc/grpc-web
-#cd grpc-web
-#git checkout refs/tags/1.0.4
-#sudo apt-get install libprotoc-dev
-#sudo make -j2 install-plugin
+#sudo snap install go
+#echo "#!/bin/bash\nexport GOPATH=$HOME/.go\nexport PATH=$PATH:$HOME/go/bin:$HOME/.go/bin">~/.bash_profile
+#go get -u github.com/golang/protobuf/protoc-gen-go
+# proxy
+#go get -u github.com/golang/dep/cmd/dep
+#git clone https://github.com/improbable-eng/grpc-web.git $GOPATH/src/github.com/improbable-eng/grpc-web
+#cd $GOPATH/src/github.com/improbable-eng/grpc-web
+#dep ensure # after installing dep
+#go install ./go/grpcwebproxy
 
-sh ./server/install.sh
-sh ./html/javascript/install.sh
+#sh ./server/install.sh
+#sh ./html/javascript/ts/install.sh
